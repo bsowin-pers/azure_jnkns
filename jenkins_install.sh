69f51102b21aa6f85631a1a511b8af201e296714
bsowin-pers <34249043+bsowin-pers@users.noreply.github.com>
2017-12-07 17:44:56 -0600

    Update jenkins_install.sh

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -8,5 +8,6 @@ sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> sources.list
 sudo apt-get update
 if ( $sudo apt-get -y install jenkins); then
   cd /usr/share/jenkins
+  sudo service jenkins stop
   java -jar jenkins.war --http8443
 fi
