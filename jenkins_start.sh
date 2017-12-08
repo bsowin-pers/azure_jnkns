4a42f1299f46e7fc909e372665607ccc9800cee2
bsowin-pers <34249043+bsowin-pers@users.noreply.github.com>
2017-12-07 18:03:58 -0600

    Update jenkins_start.sh
    Ubuntu couldn't access jenkins.war
    Swapped to service command

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -2,5 +2,6 @@
 
 if ( $(cd /usr/share/jenkins) ); then
   sudo service jenkins stop
-  sudo java -jar jenkins.war --http8080
+  sudo service jenkins start
+##  sudo java -jar jenkins.war --http8080
 fi
