367c828981a2ec7002bb1f9bca5aab99063998dc
bsowin-pers <34249043+bsowin-pers@users.noreply.github.com>
2018-01-15 14:45:03 -0600

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -1,5 +1,8 @@
 mkdir ~/jenkins
 cd ~/jenkins
 
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
+
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 java -jar jenkins.war --httpPort8081
